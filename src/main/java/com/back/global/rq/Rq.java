247741c1a296f011994e506e3dae68d685401010
sat7312 <sat7312@gmail.com>
2026-03-31 11:05:32 +0900

    45 Cookie헤더에서 인증정보 찾아서 처리

--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -9,6 +9,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 @RequiredArgsConstructor
 public class Rq {
@@ -32,15 +34,27 @@ public class Rq {
 
         String authorizationHeader = request.getHeader("Authorization");
 
-        if (authorizationHeader == null) {
-            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+        String apiKey = null;
+
+        //헤더 방식 vs 쿠키 방식
+        if (authorizationHeader != null) {
+//            throw new ServiceException("401-1", "인증 정보가 헤더에 존재하지 않습니다.");
+            if (!authorizationHeader.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "잘못된 형식의 인증데이터 입니다.");
+            }
+            apiKey = authorizationHeader.replace("Bearer ", "");
+        } else {
+            apiKey = request.getCookies() == null ? ""
+                    : Arrays.stream(request.getCookies())
+                      .filter(cookie -> cookie.getName().equals("apiKey"))
+                      .map(Cookie::getValue)
+                      .findFirst()
+                      .orElse("");
         }
 
-        if (!authorizationHeader.startsWith("Bearer ")) {
-            throw new ServiceException("401-2", "잘못된 형식의 인증데이터 입니다.");
+        if (apiKey.isBlank()) {
+            throw new ServiceException("401-3", "인증 정보가 유효하지 않습니다.");
         }
-
-        String apiKey = authorizationHeader.replace("Bearer ", "");
 
         return memberService.findByApiKey(apiKey).orElseThrow(
                 () -> new ServiceException("401-1", "유효하지 않은 API 키입니다.")
