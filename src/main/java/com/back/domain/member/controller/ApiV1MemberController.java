4263037d52327b9936cf3ce00cc9b3711fc47e1c
sat7312 <sat7312@gmail.com>
2026-03-31 10:19:37 +0900

    43 응답 쿠키 생성 코드 Rq로 이동

--- src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/back/domain/member/controller/ApiV1MemberController.java
@@ -6,7 +6,6 @@ import com.back.domain.member.service.MemberService;
 import com.back.global.exception.ServiceException;
 import com.back.global.rq.Rq;
 import com.back.global.rsData.RsData;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -67,9 +66,7 @@ public class ApiV1MemberController {
             throw new ServiceException("401-2", "비밀번호가 일치하지 않습니다.");
         }
 
-        response.addCookie(
-                new Cookie("apiKey", actor.getApiKey())
-        );
+        rq.addCookie("apiKey", actor.getApiKey());
 
         return new RsData(
                 "%s님 환영합니다.".formatted(actor.getName()),
--- src/main/java/com/back/global/rq/Rq.java
+++ src/main/java/com/back/global/rq/Rq.java
@@ -3,7 +3,9 @@ package com.back.global.rq;
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.service.MemberService;
 import com.back.global.exception.ServiceException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,14 @@ import org.springframework.stereotype.Component;
 public class Rq {
 
     private final HttpServletRequest request; // requestScope
+    private final HttpServletResponse response;
     private final MemberService memberService;
+
+    public void addCookie(String name, String value) {
+        response.addCookie(
+                new Cookie(name, value)
+        );
+    }
 
     public Member getActor() {
 
