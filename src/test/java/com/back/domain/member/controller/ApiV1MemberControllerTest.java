72715ac08ce75aec466d8591a3571a7a14f7f173
sat7312 <sat7312@gmail.com>
2026-03-31 09:46:49 +0900

    41 apiKey쿠키 생성을 위한 테스트 케이스 작성

--- src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
+++ src/test/java/com/back/domain/member/controller/ApiV1MemberControllerTest.java
@@ -3,6 +3,7 @@ package com.back.domain.member.controller;
 
 import com.back.domain.member.entity.Member;
 import com.back.domain.member.repository.MemberRepository;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -127,6 +129,15 @@ public class ApiV1MemberControllerTest {
                 .andExpect(jsonPath("$.resultCode").value("200-1"))
                 .andExpect(jsonPath("$.msg").value("%s님 환영합니다.".formatted(member.getNickname())))
                 .andExpect(jsonPath("$.data.apiKey").exists());
+
+        resultActions.andExpect(
+                result -> {
+                    Cookie apiKeyCookie = result.getResponse().getCookie("apiKey");
+
+                    assertThat(apiKeyCookie).isNotNull();
+                    assertThat(apiKeyCookie.getValue()).isNotBlank();
+                }
+        );
     }
 
     @Test
